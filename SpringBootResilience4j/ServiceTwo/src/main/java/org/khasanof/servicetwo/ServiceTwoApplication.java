21aeb65f2eef83cf67592a33bec0de48c44eb275
Nurislom373 <nurislomx72@gmail.com>
2022-10-26 22:38:33 +0500

    Add Resilience4j upgrade !1 #9

--- SpringBootResilience4j/ServiceTwo/src/main/java/org/khasanof/servicetwo/ServiceTwoApplication.java
+++ SpringBootResilience4j/ServiceTwo/src/main/java/org/khasanof/servicetwo/ServiceTwoApplication.java
@@ -3,6 +3,7 @@ package org.khasanof.servicetwo;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class ServiceTwoApplication {
@@ -62,6 +67,27 @@ class ServiceController {
         return new ResponseEntity<>("Service is Down! with retry", HttpStatus.SERVICE_UNAVAILABLE);
     }
 
+    /*
+        We can use the time limiter pattern to set a threshold timeout
+        value for async calls made to external systems.
+        Let's add the /api/time-limiter API endpoint that internally calls a slow API:
+     */
+    @RequestMapping(value = "time-limiter", method = RequestMethod.GET)
+    @TimeLimiter(name = "timeLimiterApi")
+    public ResponseEntity<String> sayTimeLimiter() throws ExecutionException, InterruptedException {
+        return new ResponseEntity<>(CompletableFuture.supplyAsync(this::callApiWithDelay).get(), HttpStatus.OK);
+    }
+
+    public String callApiWithDelay() {
+        String result = restTemplate.getForObject("/api/say", String.class);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ignore) {
+        }
+        return result;
+    }
+
+
 }
 
 @ControllerAdvice
@@ -70,6 +96,11 @@ class ApiExceptionHandler {
     @ExceptionHandler({CallNotPermittedException.class})
     public ResponseEntity<String> handleCallNotPermittedException() {
         return new ResponseEntity<>("Service is Down! with circuit-breaker", HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler({TimeoutException.class})
+    public ResponseEntity<String> handleTimeoutException() {
+        return new ResponseEntity<>("Service is Down! with time-limiter", HttpStatus.REQUEST_TIMEOUT);
     }
 
 }

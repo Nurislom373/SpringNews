b8569bd7f44694b581087fca1d2a69603fda5c7e
Nurislom <nurislomx72@gmail.com>
2024-05-26 11:13:25 -0700

    Update Postman Examples #152

--- WebSocket/WebSocketSTOMP/src/main/java/org/khasanof/websocketstomp/MessageController.java
+++ WebSocket/WebSocketSTOMP/src/main/java/org/khasanof/websocketstomp/MessageController.java
@@ -1,6 +1,7 @@
 package org.khasanof.websocketstomp;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Controller;
  * @see org.khasanof.websocketstomp
  * @since 10/1/2023 4:43 PM
  */
+@Slf4j
 @Controller
 public class MessageController {
 
     @MessageMapping("/app/greeting")
     @SendTo("/topic/messages")
     public String handle(@Payload GreetingDTO greeting) {
+        log.info("receive message : {}", greeting);
         return "[" + System.currentTimeMillis() + ": " + greeting.message;
     }
 
---
+++ WebSocket/postman/README.md
@@ -0,0 +1,7 @@
+# Postman Connection
+
+STOMP connection with postman example
+
+![con](../static/images/stomp_connection.png)
+
+ws://localhost:9999/test/123/fdgs1234/websocket

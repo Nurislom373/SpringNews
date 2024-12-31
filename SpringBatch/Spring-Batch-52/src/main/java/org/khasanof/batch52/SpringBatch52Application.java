1c2ce7aaf7215b1e238e7018b44f95c62afbd68d
Nurislom <nurislomx72@gmail.com>
2024-12-31 21:22:22 +0500

    started Spring Batch 5.2 #12-31

---
+++ SpringBatch/Spring-Batch-52/src/main/java/org/khasanof/batch52/SpringBatch52Application.java
@@ -0,0 +1,13 @@
+package org.khasanof.batch52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch52Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch52Application.class, args);
+    }
+
+}

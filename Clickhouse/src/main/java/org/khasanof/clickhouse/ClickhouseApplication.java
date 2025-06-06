220745891bda816e10f129bedf97ed18b7b4f079
Nurislom <nurislomx72@gmail.com>
2025-06-06 21:10:03 +0500

    Начал писать пример приложения на Spring для работы с базой данных ClickHouse

---
+++ Clickhouse/src/main/java/org/khasanof/clickhouse/ClickhouseApplication.java
@@ -0,0 +1,13 @@
+package org.khasanof.clickhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseApplication.class, args);
+    }
+
+}
